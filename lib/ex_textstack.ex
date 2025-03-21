cd00254770334e9a5eed2ffea19da178dc1b55e5
Sunny Rosalia <sunny@pragtechnologies.com>
2025-03-21 08:42:19 +0800

    Initial commit

---
+++ lib/ex_textstack.ex
@@ -0,0 +1,18 @@
+defmodule ExTextstack do
+  @moduledoc """
+  Documentation for `ExTextstack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTextstack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_textstack_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTextstackTest do
+  use ExUnit.Case
+  doctest ExTextstack
+
+  test "greets the world" do
+    assert ExTextstack.hello() == :world
+  end
+end
