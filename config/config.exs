1e6f9b71b90d1dbda1f8192f6506c3bd042e618d
Noel Del Castillo <noel@pragtechnologies.com>
2025-03-24 13:42:20 +0800

    Remove api secret

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ import Config
 
 config :ex_textstack,
   base_url: "https://v3-staging.textstack.io//api",
-  api_key: "idx_hQgk_-s_Ex0fyguUsIqheAGde6in4Gxx"
+  api_key: "<api-secret>"
 
 import_config "#{config_env()}.exs"
