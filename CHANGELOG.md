72d014293245b6f4d2a81576c3010f9ab4e7b41a
Noel Del Castillo <noel@pragtechnologies.com>
2025-03-24 11:33:45 +0800

    Setup docs

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## v0.1.0 (2025-03-24)
+
+- Add Send SMS api
--- mix.exs
+++ mix.exs
@@ -1,13 +1,42 @@
 defmodule ExTextstack.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @name "ExTextstack"
+  @organization "pragtechnologies"
+  @description "Elixir library for Textstack based on https://www.textstack.io/developer/api-reference"
+  @git "https://github.com/Pragtechnologies/ex_seeyoudoc"
+
   def project do
     [
       app: :ex_textstack,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Docs
+      name: @name,
+      source_url: @git,
+      homepage_url: "https://www.pragtechnologies.com/",
+      description: @description,
+      package: package(),
+      aliases: aliases(),
+      docs: [
+        # The main page in the docs
+        main: @name,
+        logo: "logo.svg",
+        extras: ["README.md", "CHANGELOG.md"]
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -24,7 +53,25 @@ defmodule ExTextstack.MixProject do
       {:req, "~> 0.5.9"},
       {:jason, "~> 1.4"},
       {:exvcr, "~> 0.17.0"},
-      {:hackney, "~> 1.23"}
+      {:hackney, "~> 1.23"},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18", only: [:test]},
+      {:credo, "~> 1.7", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      check: ["dialyzer", "credo"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @git},
+      maintainers: [@organization]
     ]
   end
 end
